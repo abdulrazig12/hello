e39aaeee1736a858f1bf558cdf009d77f76af287
Yusuf Pisan <pisan@uw.edu>
2019-01-03 13:07:22 -0800

    comments and output

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
-#include <stdio.h>
+/**
+ * Basic C program - Hello World
+ *
+ * @author Yusuf Pisan
+ * @date 7 Dec 2018
+ */
+
+ #include <stdio.h>
 
 int main() {
+  // unused variable 'i' warning from  -Wall flag
+  int i;
+  unsigned int x = 10;
+  // comparison of unsigned expression warning from -Wextra flag
+  if (x < 0) {
+    printf("Ouch!\n");
+  }
   printf("Hello, World!\n");
   return 0;
 }
